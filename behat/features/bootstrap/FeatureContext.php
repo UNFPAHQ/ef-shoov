ad091f6673fff9c9df31746445f7fc97e495a642
David Kohav <david@gizra.com>
2016-01-21 15:22:33 +0200

    Update the feature context.

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -1,12 +1,12 @@
 <?php
 
-use Drupal\DrupalExtension\Context\DrupalContext;
+use Drupal\DrupalExtension\Context\MinkContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Tester\Exception\PendingException;
 
-class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends MinkContext implements SnippetAcceptingContext {
 
   /**
    * @Given I am an anonymous user
