0233be28bc42d5e006fa9aa4dd26dac475fea5b3
david <david@gizra.com>
2016-06-29 13:19:31 +0300

    Added more delay

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -65,7 +65,7 @@ describe('Visual monitor testing', function() {
   it('should show the home page',function(done) {
     client
       .url(baseUrl)
-      .pause(2000)
+      .pause(5000)
       .webdrivercss(testName + '.homepage', {
         name: '1',
         exclude:
