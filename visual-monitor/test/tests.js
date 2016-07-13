bba06000015573b7f61ab65538fdff69a38b171b
david <david@gizra.com>
2016-07-13 17:56:17 +0300

    Update

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -65,7 +65,7 @@ describe('Visual monitor testing', function() {
   it('should show the home page',function(done) {
     client
       .url(baseUrl)
-      .pause(2497)
+      .pause(2496)
       .webdrivercss(testName + '.homepage', {
         name: '1',
         exclude:
